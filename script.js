9062ff5a091cc1b94480e8d193e912694db7772d
Pravin <128983635+pravinkr05@users.noreply.github.com>
2023-10-07 17:55:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+   // Get the current year
+   const currentYear = new Date().getFullYear();
+
+  // Find the element with the ID "currentYear" and set its content to the current year
+  document.getElementById("currentYear").textContent = currentYear;
